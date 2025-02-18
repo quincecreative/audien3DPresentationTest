8f92fa9e56881b886268e202b163f96b8085c610
quincecreative <162695957+quincecreative@users.noreply.github.com>
2025-02-18 12:30:58 +0100

    Delete assets directory

--- assets/tgaTextureLoader-qcSEG0Lr.js
+++
@@ -1,1 +0,0 @@
-import{g as r,h as n}from"./atom2PRO-CvzX0ggB.js";import"./style-kKTrETdz.js";class d{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(e,t,a){const o=new Uint8Array(e.buffer,e.byteOffset,e.byteLength),s=r(o);a(s.width,s.height,t.generateMipMaps,!1,()=>{n(t,o)})}}export{d as _TGATextureLoader};
